690dbcfef2e854dc19c35e2a9d588899dc436afa
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-03 19:17:35 +0100

    feature: add integration test folder (#12)

---
+++ packages/test-integration/src/parts/CreateFileWatcherProcess/CreateFileWatcherProcess.ts
@@ -0,0 +1,47 @@
+import { type ChildProcess, fork } from 'node:child_process'
+import { join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+const FILE_WATCHER_PROCESS_PATH = join(__dirname, '../../../../file-watcher-process/src/fileWatcherProcessMain.ts')
+
+export interface FileWatcherProcess {
+  readonly invoke: (method: string, ...params: unknown[]) => Promise<unknown>
+  readonly childProcess: ChildProcess
+  readonly [Symbol.dispose]: () => void
+}
+
+export const createFileWatcherProcess = (options: { execArgv?: string[] } = {}): FileWatcherProcess => {
+  const childProcess = fork(FILE_WATCHER_PROCESS_PATH, ['--ipc-type=node-forked-process'], {
+    execArgv: options.execArgv || ['--experimental-strip-types'],
+    stdio: 'pipe',
+  })
+  return {
+    childProcess,
+    async invoke(method: string, ...params: any[]): Promise<void> {
+      const { promise, resolve } = Promise.withResolvers<any>()
+      const messageId = Math.random()
+      const listener = (message: any): void => {
+        if (message.id === messageId) {
+          childProcess.off('message', listener)
+          resolve(message)
+        }
+      }
+      childProcess.on('message', listener)
+      childProcess.send({
+        jsonrpc: '2.0',
+        id: messageId,
+        method,
+        params,
+      })
+      const response = await promise
+      if (response.error) {
+        throw new Error(response.error.message)
+      }
+      return response.result
+    },
+    [Symbol.dispose](): void {
+      childProcess.kill()
+    },
+  }
+}
