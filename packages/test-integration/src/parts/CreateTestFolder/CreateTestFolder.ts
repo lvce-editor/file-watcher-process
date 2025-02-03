47fe974f75df84dab60c9707361efe374ee61f4c
Le Vivilet <levivlet@pm.me>
2025-02-03 19:27:54 +0100

    feature: improve file watcher integration test

---
+++ packages/test-integration/src/parts/CreateTestFolder/CreateTestFolder.ts
@@ -0,0 +1,17 @@
+import { join } from 'path'
+import * as Root from '../Root/Root.ts'
+import { randomUUID } from 'crypto'
+import { mkdir, rm } from 'fs/promises'
+
+export const createTestFolder = async () => {
+  const uuid = randomUUID()
+  const folderPath = join(Root.root, 'test-integration', uuid)
+  await rm(folderPath, { recursive: true, force: true })
+  await mkdir(folderPath, { recursive: true })
+  return {
+    folderPath,
+    async [Symbol.asyncDispose]() {
+      await rm(folderPath, { recursive: true, force: true })
+    },
+  }
+}
--- packages/test-integration/test/WatchFile.test.ts
+++ packages/test-integration/test/WatchFile.test.ts
@@ -1,8 +1,16 @@
 import { test } from '@jest/globals'
 import { createFileWatcherProcess } from '../src/parts/CreateFileWatcherProcess/CreateFileWatcherProcess.ts'
+import { createTestFolder } from '../src/parts/CreateTestFolder/CreateTestFolder.ts'
+import { writeFile } from 'fs/promises'
+import { join } from 'path'
 
 test('file watcher - watch file', async () => {
   const fileWatcherProcess = createFileWatcherProcess()
-  // TODO create a test folder, trigger a file change and verify the result is send to the process
+  const folder = await createTestFolder()
+  // TODO maybe use uris instead of file paths
+  await fileWatcherProcess.invoke('FileWatcher.watchFile', folder)
+  await writeFile(join(folder.folderPath, 'a.txt'), 'a')
+  // TODO wait for event from file watcher process
   fileWatcherProcess[Symbol.dispose]()
+  await folder[Symbol.asyncDispose]()
 })
