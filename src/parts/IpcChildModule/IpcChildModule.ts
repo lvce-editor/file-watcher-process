556ca24e176daee3060194a5bd7f27f5db102514
Le Vivilet <levivlet@pm.me>
2025-02-03 18:46:41 +0100

    feature: add basic file watcher code

---
+++ src/parts/IpcChildModule/IpcChildModule.ts
@@ -0,0 +1,23 @@
+import type { RpcClient } from '@lvce-editor/rpc'
+import {
+  ElectronMessagePortRpcClient,
+  ElectronUtilityProcessRpcClient,
+  NodeForkedProcessRpcClient,
+  NodeWorkerRpcClient,
+} from '@lvce-editor/rpc'
+import * as IpcChildType from '../IpcChildType/IpcChildType.ts'
+
+export const getModule = (method: number): RpcClient<any> => {
+  switch (method) {
+    case IpcChildType.NodeForkedProcess:
+      return NodeForkedProcessRpcClient
+    case IpcChildType.NodeWorker:
+      return NodeWorkerRpcClient
+    case IpcChildType.ElectronUtilityProcess:
+      return ElectronUtilityProcessRpcClient
+    case IpcChildType.ElectronMessagePort:
+      return ElectronMessagePortRpcClient
+    default:
+      throw new Error('unexpected ipc type')
+  }
+}
