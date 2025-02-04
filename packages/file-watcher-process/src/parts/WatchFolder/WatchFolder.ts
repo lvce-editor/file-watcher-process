0e87924428bb628052d2bcd90f1763668cfdbe7b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-04 17:43:24 +0100

    feature: add watchInternal helper function (#23)
    * feature: add watchInternal helper function
    
    * lint

--- packages/file-watcher-process/src/parts/WatchFolder/WatchFolder.ts
+++ packages/file-watcher-process/src/parts/WatchFolder/WatchFolder.ts
@@ -1,9 +1,9 @@
 import * as SharedProcess from '../SharedProcess/SharedProcess.ts'
-import * as WatchFolderInternal from '../WatchFolderInternal/WatchFolderInternal.ts'
+import * as WatchInternal from '../WatchInternal/WatchInternal.ts'
 
 export const watchFolder = async (path: string): Promise<void> => {
   const callback = async (event: any): Promise<void> => {
     await SharedProcess.invoke('FileWatcher.handleChange', event)
   }
-  await WatchFolderInternal.watchFolderInternal(path, callback)
+  await WatchInternal.watchInternal(path, { recursive: true }, callback)
 }
