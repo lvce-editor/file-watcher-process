702e0f98975bf2bb254e497ebc89610d52cdfa94
Le Vivilet <levivlet@pm.me>
2025-02-04 17:49:38 +0100

    feature: add more watchFolder integration tests

--- packages/test-integration/test/WatchFolder.test.ts
+++ packages/test-integration/test/WatchFolder.test.ts
@@ -1,10 +1,10 @@
 import { expect, test } from '@jest/globals'
 import { createFileWatcherProcess } from '../src/parts/CreateFileWatcherProcess/CreateFileWatcherProcess.ts'
 import { createTestFolder } from '../src/parts/CreateTestFolder/CreateTestFolder.ts'
-import { writeFile } from 'fs/promises'
+import { rename, rm, writeFile } from 'fs/promises'
 import { join } from 'path'
 
-test('file watcher - watch folder', async () => {
+test('watch folder - file created', async () => {
   const fileWatcherProcess = createFileWatcherProcess()
   const folder = await createTestFolder()
   // TODO maybe use uris instead of file paths
@@ -13,6 +13,32 @@ test('file watcher - watch folder', async () => {
   await writeFile(uri, 'a')
   const event = await fileWatcherProcess.nextEvent()
   expect(event).toEqual({ eventType: 'rename', filename: 'a.txt' })
+  fileWatcherProcess[Symbol.dispose]()
+  await folder[Symbol.asyncDispose]()
+})
+
+test('watch folder - file removed', async () => {
+  const fileWatcherProcess = createFileWatcherProcess()
+  const folder = await createTestFolder()
+  const uri = join(folder.folderPath, 'a.txt')
+  await writeFile(uri, 'a')
+  await fileWatcherProcess.invoke('FileWatcher.watchFolder', folder.folderPath)
+  await rm(uri)
+  const event = await fileWatcherProcess.nextEvent()
+  expect(event).toEqual({ eventType: 'rename', filename: 'a.txt' })
+  fileWatcherProcess[Symbol.dispose]()
+  await folder[Symbol.asyncDispose]()
+})
+
+test('watch folder - file renamed', async () => {
+  const fileWatcherProcess = createFileWatcherProcess()
+  const folder = await createTestFolder()
+  const uri = join(folder.folderPath, 'a.txt')
+  await writeFile(uri, 'a')
+  await fileWatcherProcess.invoke('FileWatcher.watchFolder', folder.folderPath)
+  await rename(uri, join(folder.folderPath, 'b.txt'))
+  const event = await fileWatcherProcess.nextEvent()
+  expect(event).toEqual({ eventType: 'rename', filename: 'b.txt' })
   fileWatcherProcess[Symbol.dispose]()
   await folder[Symbol.asyncDispose]()
 })
