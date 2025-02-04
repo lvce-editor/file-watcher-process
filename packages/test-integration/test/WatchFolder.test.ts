06475903029fdab3c1b196d6e39f690f8a5b01dd
Le Vivilet <levivlet@pm.me>
2025-02-04 22:16:03 +0100

    test

--- packages/test-integration/test/WatchFolder.test.ts
+++ packages/test-integration/test/WatchFolder.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@jest/globals'
 import { createFileWatcherProcess } from '../src/parts/CreateFileWatcherProcess/CreateFileWatcherProcess.ts'
 import { createTestFolder } from '../src/parts/CreateTestFolder/CreateTestFolder.ts'
-import { writeFile } from 'fs/promises'
+import { rename, rm, writeFile } from 'fs/promises'
 import { join } from 'path'
 
 test('file watcher - watch folder', async () => {
@@ -11,6 +11,32 @@ test('file watcher - watch folder', async () => {
   await fileWatcherProcess.invoke('FileWatcher.watchFolder', folder.folderPath)
   const uri = join(folder.folderPath, 'a.txt')
   await writeFile(uri, 'a')
+  const event = await fileWatcherProcess.nextEvent()
+  expect(event).toEqual({ eventName: 'addDir', path: folder.folderPath })
+  fileWatcherProcess[Symbol.dispose]()
+  await folder[Symbol.asyncDispose]()
+})
+
+test('watch folder - file removed', async () => {
+  const fileWatcherProcess = createFileWatcherProcess()
+  const folder = await createTestFolder()
+  const uri = join(folder.folderPath, 'a.txt')
+  await writeFile(uri, 'a')
+  await fileWatcherProcess.invoke('FileWatcher.watchFolder', folder.folderPath)
+  await rm(uri)
+  const event = await fileWatcherProcess.nextEvent()
+  expect(event).toEqual({ eventName: 'addDir', path: folder.folderPath })
+  fileWatcherProcess[Symbol.dispose]()
+  await folder[Symbol.asyncDispose]()
+})
+
+test('watch folder - file renamed', async () => {
+  const fileWatcherProcess = createFileWatcherProcess()
+  const folder = await createTestFolder()
+  const uri = join(folder.folderPath, 'a.txt')
+  await writeFile(uri, 'a')
+  await fileWatcherProcess.invoke('FileWatcher.watchFolder', folder.folderPath)
+  await rename(uri, join(folder.folderPath, 'b.txt'))
   const event = await fileWatcherProcess.nextEvent()
   expect(event).toEqual({ eventName: 'addDir', path: folder.folderPath })
   fileWatcherProcess[Symbol.dispose]()
