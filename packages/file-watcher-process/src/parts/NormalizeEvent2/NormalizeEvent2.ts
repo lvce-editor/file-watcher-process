678d9a3057b4370b1aa4600e2f404318b336daa6
Le Vivilet <levivlet@pm.me>
2025-02-05 00:03:02 +0100

    lint

--- packages/file-watcher-process/src/parts/NormalizeEvent2/NormalizeEvent2.ts
+++ packages/file-watcher-process/src/parts/NormalizeEvent2/NormalizeEvent2.ts
@@ -1,5 +1,5 @@
 import { pathToFileURL } from 'node:url'
-import { NormalizedEvent2 } from '../NormalizedEvent2/NormalizedEvent2.ts'
+import type { NormalizedEvent2 } from '../NormalizedEvent2/NormalizedEvent2.ts'
 
 export const normalizeEvent2 = (id: number, eventName: string, path: string): NormalizedEvent2 => {
   const uri = pathToFileURL(path).toString()
