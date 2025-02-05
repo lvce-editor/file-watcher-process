c5d5da27bdd06b8c02b0cacc45713331b0ada81e
Le Vivilet <levivlet@pm.me>
2025-02-05 20:48:10 +0100

    lint

--- packages/file-watcher-process/src/parts/HandleElectronMessagePort/HandleElectronMessagePort.ts
+++ packages/file-watcher-process/src/parts/HandleElectronMessagePort/HandleElectronMessagePort.ts
@@ -1,7 +1,7 @@
 import * as IpcChild from '../IpcChild/IpcChild.ts'
 import * as IpcChildType from '../IpcChildType/IpcChildType.ts'
 
-export const handleElectronMessagePort = async (messagePort: any) => {
+export const handleElectronMessagePort = async (messagePort: any): Promise<void> => {
   await IpcChild.listen({
     method: IpcChildType.ElectronMessagePort,
     messagePort,
