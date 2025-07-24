8865fced3ec54c12a1b1403086354804dbea8ee7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-24 21:45:00 +0200

    feature: move callbacks up (#90)
    * feature: move callbacks up
    
    * add error callback

--- packages/file-watcher-process/src/parts/WatchFolder/WatchFolder.ts
+++ packages/file-watcher-process/src/parts/WatchFolder/WatchFolder.ts
@@ -4,18 +4,24 @@ import * as NormalizeEvent from '../NormalizeEvent/NormalizeEvent.ts'
 import * as SharedProcess from '../SharedProcess/SharedProcess.ts'
 import * as WaitForWatcherToBeReady from '../WaitForWatcherToBeReady/WaitForWatcherToBeReady.ts'
 
-export const watchFolder = async (uri: string): Promise<void> => {
-  const callBackInternal = async (eventName: string, path: string, stats: any): Promise<void> => {
-    const event = NormalizeEvent.normalizeEvent(eventName, path)
-    await SharedProcess.invoke('FileWatcher.handleChange', event)
-  }
-  const callback = (eventName: string, path: string, stats: any): void => {
-    void callBackInternal(eventName, path, stats)
-  }
+const callBackInternal = async (eventName: string, path: string, stats: any): Promise<void> => {
+  const event = NormalizeEvent.normalizeEvent(eventName, path)
+  await SharedProcess.invoke('FileWatcher.handleChange', event)
+}
 
+const callback = (eventName: string, path: string, stats: any): void => {
+  void callBackInternal(eventName, path, stats)
+}
+
+const errorCallback = (error: any): void => {
+  console.error(`[file-watcher-process] ${error}`)
+}
+
+export const watchFolder = async (uri: string): Promise<void> => {
   const path = fileURLToPath(uri)
   const watcher = watch(path)
   watcher.on('all', callback)
+  watcher.on('error', errorCallback)
 
   await WaitForWatcherToBeReady.waitForWatcherToBeReady(watcher)
 }
