f51dc1f875805806d51ab95b9c96dc32cadce701
Le Vivilet <levivilet@pm.me>
2025-11-07 13:42:57 +0100

    feature: convert file url to path

--- packages/file-watcher-process/src/parts/WatchFile/WatchFile.ts
+++ packages/file-watcher-process/src/parts/WatchFile/WatchFile.ts
@@ -1,6 +1,7 @@
 import * as Assert from '@lvce-editor/assert'
 import * as SharedProcess from '../SharedProcess/SharedProcess.ts'
 import * as WatchInternal from '../WatchInternal/WatchInternal.ts'
+import { fileURLToPath } from 'node:url'
 
 export const watchFile = async (path: string): Promise<void> => {
   const callback = async (event: any): Promise<void> => {
@@ -18,5 +19,6 @@ export const watchFile2 = async (id: number, uri: string): Promise<void> => {
       ...event,
     })
   }
-  await WatchInternal.watchInternal(uri, {}, callback)
+  const path = fileURLToPath(uri)
+  await WatchInternal.watchInternal(path, {}, callback)
 }
