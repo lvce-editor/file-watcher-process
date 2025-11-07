826a2f721d1ad966783d282ea4e6add0ef5a3c9f
Le Vivilet <levivilet@pm.me>
2025-11-07 18:06:37 +0100

    feature: add watcher dispose (#122)
    * feature: add watcher dispose
    
    * condition
    
    * lint

--- packages/file-watcher-process/src/parts/CommandMap/CommandMap.ts
+++ packages/file-watcher-process/src/parts/CommandMap/CommandMap.ts
@@ -1,12 +1,14 @@
+import * as DisposeWatcher from '../DisposeWatcher/DisposeWatcher.ts'
 import * as HandleElectronMessagePort from '../HandleElectronMessagePort/HandleElectronMessagePort.ts'
 import * as WatchFile from '../WatchFile/WatchFile.ts'
 import * as WatchFolder from '../WatchFolder/WatchFolder.ts'
 import * as WatchFolders from '../WatchFolders/WatchFolders.ts'
 
 export const commandMap = {
-  'HandleElectronMessagePort.handleElectronMessagePort': HandleElectronMessagePort.handleElectronMessagePort,
+  'FileWatcher.dispose': DisposeWatcher.disposeWatcher,
   'FileWatcher.watchFile': WatchFile.watchFile,
   'FileWatcher.watchFile2': WatchFile.watchFile2,
   'FileWatcher.watchFolder': WatchFolder.watchFolder,
   'FileWatcher.watchFolders': WatchFolders.watchFolders,
+  'HandleElectronMessagePort.handleElectronMessagePort': HandleElectronMessagePort.handleElectronMessagePort,
 }
--- packages/file-watcher-process/src/parts/WatchFile/WatchFile.ts
+++ packages/file-watcher-process/src/parts/WatchFile/WatchFile.ts
@@ -1,6 +1,7 @@
 import * as Assert from '@lvce-editor/assert'
 import { fileURLToPath } from 'node:url'
 import * as SharedProcess from '../SharedProcess/SharedProcess.ts'
+import * as WatcherState from '../WatcherState/WatcherState.ts'
 import * as WatchInternal from '../WatchInternal/WatchInternal.ts'
 
 export const watchFile = async (path: string): Promise<void> => {
@@ -20,5 +21,7 @@ export const watchFile2 = async (id: number, uri: string): Promise<void> => {
     })
   }
   const path = fileURLToPath(uri)
+  const controller = new AbortController()
+  WatcherState.add(id, controller)
   await WatchInternal.watchInternal(path, {}, callback)
 }
