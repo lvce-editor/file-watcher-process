45a3313db2a5e1cc7ebb8b6191f714ecd96e02fa
Le Vivilet <levivilet@pm.me>
2026-07-10 16:49:52 +0200

    fix: route electron file watcher events (#126)
    * fix: route electron file watcher events
    
    * test: isolate electron ipc type

--- packages/file-watcher-process/src/parts/HandleElectronMessagePort/HandleElectronMessagePort.ts
+++ packages/file-watcher-process/src/parts/HandleElectronMessagePort/HandleElectronMessagePort.ts
@@ -1,9 +1,11 @@
 import * as IpcChild from '../IpcChild/IpcChild.ts'
 import * as IpcChildType from '../IpcChildType/IpcChildType.ts'
+import * as RpcRegistry from '../RpcRegistry/RpcRegistry.ts'
 
-export const handleElectronMessagePort = async (messagePort: any): Promise<void> => {
-  await IpcChild.listen({
+export const handleElectronMessagePort = async (messagePort: any, rpcId: number): Promise<void> => {
+  const rpc = await IpcChild.listen({
     messagePort,
     method: IpcChildType.ElectronMessagePort,
   })
+  RpcRegistry.set(rpcId, rpc)
 }
---
+++ packages/file-watcher-process/test/HandleElectronMessagePort.test.ts
@@ -0,0 +1,42 @@
+import { expect, jest, test } from '@jest/globals'
+
+const mockRpc = {
+  dispose: jest.fn(),
+  invoke: jest.fn(),
+  invokeAndTransfer: jest.fn(),
+  send: jest.fn(),
+}
+
+jest.unstable_mockModule('../src/parts/IpcChild/IpcChild.ts', () => {
+  return {
+    listen: jest.fn(() => mockRpc),
+  }
+})
+
+jest.unstable_mockModule('../src/parts/IpcChildType/IpcChildType.ts', () => {
+  return {
+    ElectronMessagePort: 4,
+  }
+})
+
+const IpcChild = await import('../src/parts/IpcChild/IpcChild.ts')
+const IpcChildType = await import('../src/parts/IpcChildType/IpcChildType.ts')
+const RpcRegistry = await import('../src/parts/RpcRegistry/RpcRegistry.ts')
+const HandleElectronMessagePort = await import('../src/parts/HandleElectronMessagePort/HandleElectronMessagePort.ts')
+
+test('handleElectronMessagePort - registers rpc', async () => {
+  const messagePort = {}
+  const rpcId = 999
+
+  try {
+    await HandleElectronMessagePort.handleElectronMessagePort(messagePort, rpcId)
+
+    expect(IpcChild.listen).toHaveBeenCalledWith({
+      messagePort,
+      method: IpcChildType.ElectronMessagePort,
+    })
+    expect(RpcRegistry.get(rpcId)).toBe(mockRpc)
+  } finally {
+    RpcRegistry.remove(rpcId)
+  }
+})
